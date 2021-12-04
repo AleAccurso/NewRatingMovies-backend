f042d911603bc6e86d724d7fcdb089dddfd75d3f
Medici <alessandro.accurso@epitech.eu>
2021-12-04 22:07:31 +0100

    model - release date type

--- models/movieModel.js
+++ models/movieModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const MovieSchema = new Schema({
   movieDbId: { type: String, required: true, unique: true },
   title: { type: String, required: true },
-  release_date: { type: String, required: true },
+  release_date: { type: Date, required: true },
   strGenres: { type: String, required: true },
   overview: { type: String, required: true },
   director: { type: String, required: true },
