398826939826b03440719f43a07e72c79e3ce311
SignMedici <amenophis170981@gmail.com>
2022-02-19 04:14:09 +0100

    remove cors

--- app.js
+++ app.js
@@ -15,11 +15,6 @@ const movieRouter = require("./routes/movieRouter");
 
 const app = express();
 
-// Manage HTTP requests
-const cors = require('cors');
-app.use(cors({
-  origin: '*'
-}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
