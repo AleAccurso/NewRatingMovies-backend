c6340a73bd08a4b5f06667f2a2d7ff1cd5c725c4
SignMedici <amenophis170981@gmail.com>
2022-05-10 20:25:59 +0200

    test keep alive

--- database/database.js
+++ database/database.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { wakeDyno, wakeDynos } = require("heroku-keep-awake");
 
 exports.Initialise = async (app) => {
   mongoose
@@ -7,7 +8,17 @@ exports.Initialise = async (app) => {
       useUnifiedTopology: true,
     })
     .then((result) => {
-      app.listen(process.env.PORT, () => console.log("Server started."));
+      app.listen(process.env.PORT, () => {
+        console.log("Server started.");
+
+        const opts = {
+          interval: 1,
+          logging: false,
+          stopTimes: { start: "00:00", end: "06:00" },
+        };
+
+        wakeDyno(process.env.FRONT_URL, opts);
+      });
     })
     .catch((err) => console.log(err));
 };
